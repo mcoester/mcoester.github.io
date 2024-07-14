32a692cf8f37b33cc83ed72fd4457c3a63a1854e
Michael Cöster <michaelcoester@googlemail.com>
2024-07-14 19:51:35 +0200

    Add functionality for changing the color to the color-change-button. Still not ready. There has to do some more adjustments to the colors in dark-mode.

--- script.js
+++ script.js
@@ -25,15 +25,42 @@ let bool = true;
 const switchColor = () =>{
     let backgroundWidth = getComputedStyle(background).width;
     let circleWidth = getComputedStyle(circle).width;
+    let body = document.querySelector('body');
+    let ipadSides = document.getElementById('outer-smartphone-sides');
+    let header = document.querySelector('header');
+    let main = document.querySelector('main');
+    let leftContainer = document.getElementById('left-container');
+    let projects = document.getElementById('projects');
+    let skills = document.getElementById('skills');
 
     if(bool){
         circle.style.right = circleWidth;
+        body.style.backgroundColor = 'hsl(0, 0%, 35%)';
+        console.log(getComputedStyle(body).backgroundColor);
+        main.style.backgroundColor = 'hsl(0, 0%, 35%)';
         bool = false;
     } else{
         circle.style.right = backgroundWidth;
+        body.style.backgroundColor = 'hsl(106, 33%, 90%)';
+        main.style.backgroundColor = 'hsl(106, 33%, 90%)';
         bool = true;
     }
 }
 
 background.addEventListener('click', switchColor);
 circle.addEventListener('click', switchColor);
+
+//window size-change-reaction-function for color-mode-button
+
+const sizeChange = () => {
+    let backgroundWidth = getComputedStyle(background).width;
+    let circleWidth = getComputedStyle(circle).width;
+
+    if(bool){
+        circle.style.right = backgroundWidth;
+    } else{
+        circle.style.right = circleWidth;
+    }
+}
+
+window.addEventListener('resize', sizeChange);
